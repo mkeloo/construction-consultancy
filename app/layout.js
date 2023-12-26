2435501eabb46ce2c4e2900f1ddafeb32fdd65dd
mkeloo <mkeloo@ufl.edu>
2023-12-26 14:28:53 -0500

    added the go back home btn temporarily.

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from 'next/font/google';
 import './globals.css';
+import Link from 'next/link';
+import { Button } from '@/components/ui/button';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -11,7 +13,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        {/* Temporary go back Home button */}
+        <div className="w-full h-[100vh] flex justify-center items-center">
+          <Button className="m-4 active:scale-95 transition duration-100">
+            <Link href="/">Go back home</Link>
+          </Button>
+        </div>
+      </body>
     </html>
   );
 }
